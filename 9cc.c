3f2bec34e749a9267ebb1a3e7095b8e82f8a3491
nuts3745 <japanbik@gmail.com>
2022-08-14 06:42:00 +0000

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,29 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
+  printf(".globl main\n\n");
   printf("main:\n");
-  printf("	mov rax, %d\n", atoi(argv[1]));
-  printf("	ret\n");
+  printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+  
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+  printf("\tret\n");
   return 0;
 }

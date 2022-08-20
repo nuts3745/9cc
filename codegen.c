3c7051ba65650c462a69ad077da776bba6e8a472
nuts3745 <japanbik@gmail.com>
2022-08-20 13:38:56 +0000

    文末を示すと単一の変数に対応

---
+++ codegen.c
@@ -0,0 +1,114 @@
+#include "9cc.h"
+
+static int depth;
+
+static void push(void) {
+  printf("  push %%rax\n");
+  depth++;
+}
+
+static void pop(char *arg) {
+  printf("  pop %s\n", arg);
+  depth--;
+}
+
+static void gen_addr(Node *node) {
+  if (node->kind == ND_LVAR) {
+    int offset = (node->name - 'a' + 1) * 8;
+    printf("\tlea %d(%%rbp), %%rax\n", -offset);
+    return;
+  }
+
+  error("not an lvalue");
+}
+
+static void gen_expr(Node *node) {
+  switch (node->kind) {
+  case ND_NUM:
+    printf("  mov $%d, %%rax\n", node->val);
+    return;
+  case ND_NEG:
+    gen_expr(node->lhs);
+    printf("  neg %%rax\n");
+    return;
+  case ND_LVAR:
+    gen_addr(node);
+    printf("\tmov (%%rax), %%rax\n");
+    return;
+  case ND_ASSIGN:
+    gen_addr(node->lhs);
+    push();
+    gen_expr(node->rhs);
+    pop("%rdi");
+    printf("\tmov %%rax, (%%rdi)\n");
+    return;
+  }
+
+
+  gen_expr(node->rhs);
+  push();
+  gen_expr(node->lhs);
+  pop("%rdi");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add %%rdi, %%rax\n");
+    return;
+  case ND_SUB:
+    printf("  sub %%rdi, %%rax\n");
+    return;
+  case ND_MUL:
+    printf("  imul %%rdi, %%rax\n");
+    return;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv %%rdi\n");
+    return;
+  case ND_EQ:
+  case ND_NE:
+  case ND_LT:
+  case ND_LE:
+    printf("  cmp %%rdi, %%rax\n");
+
+    if (node->kind == ND_EQ)
+      printf("  sete %%al\n");
+    else if (node->kind == ND_NE)
+      printf("  setne %%al\n");
+    else if (node->kind == ND_LT)
+      printf("  setl %%al\n");
+    else if (node->kind == ND_LE)
+      printf("  setle %%al\n");
+
+    printf("  movzb %%al, %%rax\n");
+    return;
+  }
+
+  error("invalid expression");
+}
+
+static void gen_stmt(Node *node) {
+  if (node->kind == ND_EXPR_STMT) {
+    gen_expr(node->lhs);
+    return;
+  }
+
+  error("invalid statement");
+}
+
+void codegen(Node *node) {
+  printf("  .globl main\n");
+  printf("main:\n");
+
+  printf("\tpush %%rbp\n");
+  printf("\tmov %%rsp, %%rbp\n");
+  printf("\tsub $208, %%rsp\n");
+
+  for (Node *n = node; n; n = n->next) {
+    gen_stmt(n);
+    assert(depth == 0);
+  }
+
+  printf("\tmov %%rbp, %%rsp\n");
+  printf("\tpop %%rbp\n");
+  printf("  ret\n");
+}
